dd79837f3382af5f80b17f2cfadf18dece565efc
Clarissa Soares <clah@ufrn.edu.br>
2019-07-18 21:27:57 -0300

    CRUD denuncia

--- src/main/java/br/imd/hackfest2019/MoneyWatchers/controller/DenunciaController.java
+++ src/main/java/br/imd/hackfest2019/MoneyWatchers/controller/DenunciaController.java
@@ -3,16 +3,16 @@ package br.imd.hackfest2019.MoneyWatchers.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.imd.hackfest2019.MoneyWatchers.domain.Denuncia;
 import br.imd.hackfest2019.MoneyWatchers.service.DenunciaService;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/denuncia")
 public class DenunciaController {
 	
--- src/main/java/br/imd/hackfest2019/MoneyWatchers/service/DenunciaService.java
+++ src/main/java/br/imd/hackfest2019/MoneyWatchers/service/DenunciaService.java
@@ -3,10 +3,12 @@ package br.imd.hackfest2019.MoneyWatchers.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.imd.hackfest2019.MoneyWatchers.domain.Denuncia;
 import br.imd.hackfest2019.MoneyWatchers.repository.DenunciaRepository;
 
+@Service
 public class DenunciaService {
 
 	@Autowired
